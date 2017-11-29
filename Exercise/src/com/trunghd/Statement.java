9283a0ea5e0de71a3c44fa8504178ab89da00d34
Hoang Duc Trung <hoangductrungpc@gmail.com>
2017-11-29 17:21:06 +0700

    change 2

--- Exercise/src/com/trunghd/Statement.java
+++ Exercise/src/com/trunghd/Statement.java
@@ -5,6 +5,7 @@ public class Statement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("10000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+		System.out.println("10000 at 3% interest = " + calculateInterest(10000.0, 3.0));
 	}
 	public static double calculateInterest(double amount, double interestRate) {
 		return (amount * (interestRate / 100));
