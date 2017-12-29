4bdcbbe5becaf93e860d4f5b26169a8b4c944ade
Hoang Duc Trung <hoangductrungpc@gmail.com>
2017-12-29 14:34:26 +0700

    update 29/12

---
+++ Final/src/com/trunghd/SomeClass.java
@@ -0,0 +1,18 @@
+package com.trunghd;
+
+public class SomeClass {
+    private static int classCounter = 0;
+    public final int instanceNumber;
+    private final String name;
+
+    public SomeClass(String name) {
+        this.name = name;
+        classCounter++;
+        instanceNumber = classCounter;
+        System.out.println(name + " create, instance is " + instanceNumber);
+    }
+
+    public int getInstanceNumber() {
+        return instanceNumber;
+    }
+}
